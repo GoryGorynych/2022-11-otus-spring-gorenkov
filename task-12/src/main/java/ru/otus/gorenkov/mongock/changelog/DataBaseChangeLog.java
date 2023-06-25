105ab9323406067ffd77610992b37eabfe0bffeb
Vladimit <gory.gorynych@gmail.com>
2023-06-25 23:29:53 +0300

    task-12

--- task-12/src/main/java/ru/otus/gorenkov/mongock/changelog/DataBaseChangeLog.java
+++ task-12/src/main/java/ru/otus/gorenkov/mongock/changelog/DataBaseChangeLog.java
@@ -3,9 +3,16 @@ package ru.otus.gorenkov.mongock.changelog;
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.mongodb.client.MongoDatabase;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.otus.gorenkov.models.User;
 import ru.otus.gorenkov.mongock.DataGenerator;
 import ru.otus.gorenkov.repositories.BookRepository;
 import ru.otus.gorenkov.repositories.CommentRepository;
+import ru.otus.gorenkov.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @ChangeLog
 public class DataBaseChangeLog {
@@ -23,5 +30,13 @@ public class DataBaseChangeLog {
     @ChangeSet(order = "002", id = "002", author = "vgorenkov")
     public void addComents(CommentRepository commentRepository) {
         commentRepository.saveAll(DataGenerator.getAllComments());
+    }
+
+    @ChangeSet(order = "003", id = "003", author = "vgorenkov")
+    public void addUsers(UserRepository userRepository, PasswordEncoder encoder) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(null, "user", encoder.encode("user"), Set.of("USER")));
+        userList.add(new User(null, "admin", encoder.encode("admin"), Set.of("ADMIN")));
+        userRepository.saveAll(userList);
     }
 }
